bc799de8a1276f6543f55e0ac7491c6b5bd83e21
Salma Salah <salmasalahmahmoud@gmail.com>
2020-02-20 14:07:43 +0200

    adding filter petitions

--- WhiteHousePetitions/WhiteHousePetitions/TableViewController.swift
+++ WhiteHousePetitions/WhiteHousePetitions/TableViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class TableViewController: UITableViewController {
 
     var petitions = [Petition]()
+    var filteredPeitions = [Petition]()
     override func viewDidLoad() {
         super.viewDidLoad()
         getPetitions()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(getCredits))
-        
-        // Do any additional setup after loading the view.
+        let filterBtn = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterPetitions))
+        let resetBtn = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(resetPetitions))
+        navigationItem.leftBarButtonItems = [filterBtn,resetBtn]
     }
     
     func getPetitions(){
@@ -41,11 +43,38 @@ class TableViewController: UITableViewController {
         present(ac,animated: true)
     }
     
+    @objc func filterPetitions(){
+        let ac = UIAlertController(title: "Filter Petitions", message: "Enter The Petition Title You Want", preferredStyle: .alert)
+        ac.addTextField { (tf) in
+            tf.placeholder = "Enter Petition Title here .."
+        }
+        ac.addAction(UIAlertAction(title: "Submit", style: .default, handler: { [weak self](alertAction) in
+            if let petitions = self?.petitions{
+                self?.filteredPeitions.removeAll()
+                for petition in petitions{
+                    if let enteredPetitionTitle = ac.textFields?.first?.text?.lowercased(){
+                        if petition.title.lowercased().contains(enteredPetitionTitle){
+                            self?.filteredPeitions.append(petition)
+                        }
+                    }
+                }
+                self?.tableView.reloadData()
+            }
+        }))
+        present(ac,animated: true)
+    }
+    
+    @objc func resetPetitions(){
+        filteredPeitions = petitions
+        tableView.reloadData()
+    }
+    
     func parse(data:Data){
         let decoder = JSONDecoder()
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: data){
             //nb : Petitions.self which is Swift’s way of referring to the Petitions type itself rather than an instance of it. That is, we’re not saying “create a new one”, but instead specifying it as a parameter to the decoding so JSONDecoder knows what to convert the JSON too.
             petitions = jsonPetitions.results
+            filteredPeitions = petitions
             tableView.reloadData()
         }
     }
@@ -57,11 +86,11 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPeitions.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let petition = petitions[indexPath.row]
+        let petition = filteredPeitions[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
@@ -70,7 +99,7 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailsVC = DetailsViewController()
-        detailsVC.detailItem = petitions[indexPath.row]
+        detailsVC.detailItem = filteredPeitions[indexPath.row]
         navigationController?.pushViewController(detailsVC, animated: true)
         //here we didn't use instantiate VC because we didn't add the details VC in storyboard
     }
